188b5ffec56a646b376469407a7eab422a94d370
Joyce Lee <joycelee901@gmail.com>
2021-06-14 12:28:17 -0500

    commit tree bb502a4550c6be0d67d86eb2cf28e11585183efb

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
 // create your App component here
+import { useState, useEffect } from 'react';
+
+function App() {
+    const [image, setImage] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        console.log("Fetching data...");
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then((response) => response.json())
+        .then((data) => {
+            setImage(data.message)
+            setIsLoaded(true)
+        });
+    }, []);
+
+    return (
+        <div>
+            <img 
+                alt="A Random Dog" 
+                src={image}
+            />
+            {!isLoaded ? "Loading..." : null}
+        </div>
+    )
+
+}
+
+export default App;
